2bacc2c8bf09614c155bf80b33dbd89f50dc97fb
Ri-Hong <riri.hong@gmail.com>
2023-12-28 15:39:13 -0500

    Removed docs folder

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,26 +1,11 @@
 /** @jsxImportSource theme-ui */
 
-import { Box, Flex, Link, useColorMode, jsx } from "theme-ui"
+import { Box, Flex } from "theme-ui"
 
 const Footer = () => {
-  const [colorMode, setColorMode] = useColorMode<"light" | "dark">()
-  const isDark = colorMode === `dark`
 
   return (
     <Box as="footer" variant="footer">
-      <button
-        sx={{ variant: `buttons.toggle`, fontWeight: `semibold`, display: `block`, mx: `auto`, mb: 3 }}
-        onClick={() => {
-          const next = isDark ? `light` : `dark`
-          setColorMode(next)
-          document.documentElement.classList.value = `theme-ui-${next}`
-        }}
-        type="button"
-        data-testid="color-mode-toggle"
-        aria-label={isDark ? `Activate Light Mode` : `Activate Dark Mode`}
-      >
-        {isDark ? `Light` : `Dark`}
-      </button>
       Copyright &copy; {new Date().getFullYear()}. All rights reserved.
       <br />
       <Flex
