2c72786788920031709c9f3822bef53eebbf92e6
Ri-Hong <riri.hong@gmail.com>
2024-01-12 09:53:06 -0500

    Updated resume.pdf

--- src/components/conditional-scrollbar.tsx
+++ src/components/conditional-scrollbar.tsx
@@ -8,9 +8,9 @@ const ConditionalScrollbar = () => {
     // Because we need the navbar to be transparent on the homepage AND the blog page, we need to target different compoenents. The blog page doesn't have a parallax container
     const parallaxContainer = document.querySelector(".parallax"); // replace with your container's selector
     const scrollContainer = document.documentElement; // Targets the html tag
+    const navbar = document.querySelector(".navbar");
 
     const handleScrollHomePage = () => {
-      const navbar = document.querySelector(".navbar");
       if (parallaxContainer.scrollTop > 375) { // Adjust this threshold as needed
         navbar.classList.add("scrolled");
       } else {
@@ -19,7 +19,6 @@ const ConditionalScrollbar = () => {
     };
 
     const handleScrollBlogPage = () => {
-      const navbar = document.querySelector(".navbar");
       console.log(scrollContainer.scrollTop)
       if (scrollContainer.scrollTop > 350) { // Adjust this threshold as needed
         navbar.classList.add("scrolled");
@@ -29,7 +28,7 @@ const ConditionalScrollbar = () => {
     };
 
 
-    if (location.pathname === "/") {
+    if (location.pathname === "/" && parallaxContainer) {
       // Add the event listener to the parallax container
       parallaxContainer.addEventListener("scroll", handleScrollHomePage);
     } else if (location.pathname === "/blog/") {
@@ -41,6 +40,9 @@ const ConditionalScrollbar = () => {
     return () => {
       if (parallaxContainer) {
         parallaxContainer.removeEventListener("scroll", handleScrollHomePage);
+      }
+      if (location.pathname === "/blog/") {
+        scrollContainer.removeEventListener("scroll", handleScrollBlogPage);
       }
     };
   }, [location.pathname]); // Dependency array
